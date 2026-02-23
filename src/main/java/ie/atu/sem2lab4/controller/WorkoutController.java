d63dcd0e722874bc7065f5b2beb36f90357e8157
RonsJose <g00464215@atu.ie>
2026-02-23 10:58:42 +0000

    Added Workout and exercise validation and error handling

---
+++ src/main/java/ie/atu/sem2lab4/controller/WorkoutController.java
@@ -0,0 +1,33 @@
+package ie.atu.sem2lab4.controller;
+
+import ie.atu.sem2lab4.model.Workout;
+import ie.atu.sem2lab4.service.WorkoutService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/workouts")
+public class WorkoutController {
+    private final WorkoutService service;
+
+    public WorkoutController(WorkoutService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Workout> createWorkout(
+            @Valid @RequestBody Workout workout){
+
+        Workout newWorkout = service.createWorkout(workout);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newWorkout);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Workout>> getAllWorkouts() {
+        return ResponseEntity.ok(service.getAllWorkouts());
+    }
+}
---
+++ src/main/java/ie/atu/sem2lab4/model/Exercise.java
@@ -0,0 +1,23 @@
+package ie.atu.sem2lab4.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Exercise {
+    @NotBlank(message ="Exercise name cannot be blank")
+    private String name;
+
+    @Positive(message = "Weight must be greater than 0")
+    private float weight;
+
+    @Positive(message = "Reps must be greater than 0")
+    private int reps;
+
+    @Positive(message = "Sets must be greater than 0")
+    private int sets;
+
+}
